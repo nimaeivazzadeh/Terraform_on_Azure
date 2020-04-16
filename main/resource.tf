a289a65b3df9c77220d2af45af72aaa6441871df
nimaeivazzadeh <nima.eivazzadehkaljahi@yahoo.com>
2020-04-16 20:30:55 +0200

    virtual network update

---
+++ main/resource.tf
@@ -0,0 +1,16 @@
+resource "azurerm_resource_group" "web_server_region" {
+  name     = var.region
+  location = var.web_server_location
+}
+
+resource "azurerm_virtual_network" "web_server_vnet"  {
+  name                = "${var.resource_prefix}-vnet"
+  location            = var.web_server_location
+  resource_group_name = azurerm_resource_group.web_server_region.name
+  address_space       = [var.web_server_address_space]
+  
+}
+
+
+
+
--- main/vars.tf
+++ main/vars.tf
@@ -1,8 +1,18 @@
-variable "rg" {
-    # type = string
+variable "region" {
+    type    = string
     default = "Oslo"
 }
-variable "location" {
-    # type = string
+variable "web_server_location" {
+    type    = string
     default = "Norway East"
+}
+
+variable "resource_prefix" {
+    type    = string
+    default = "web-server"
+}
+
+variable "web_server_address_space" {
+    type    = string
+    default = "1.0.0.0/22"
 }
