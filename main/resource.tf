91da48ac217fa6c50b5443aff37e9ffad42b90d9
nimaeivazzadeh <nima.eivazzadehkaljahi@yahoo.com>
2020-04-17 11:34:54 +0200

    network interface and its association update

--- main/resource.tf
+++ main/resource.tf
@@ -19,5 +19,51 @@ resource "azurerm_subnet" "web_server_subnet" {
 
 }
 
+resource "azurerm_network_interface" "web_server_nic" { 
+  name                 = "${var.web_server_name}-nic"
+  location             =  var.web_server_location
+  resource_group_name  =  azurerm_resource_group.web_server_region.name
 
+  ip_configuration {
+    name               = "${var.web_server_name}-ip"
+    subnet_id          = azurerm_subnet.web_server_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+}
+
+
+resource "azurerm_public_ip" "web_server_public_ip" {
+  name                 = "${var.resource_prefix}-public-ip"
+  location             = var.web_server_location
+  resource_group_name  = azurerm_resource_group.web_server_region.name
+  allocation_method    = var.environment == "Production" ? "Static" : "Dynamic"   # a conditional statement.
+}
+
+resource "azurerm_network_security_group" "web_server_nsg" {
+  name                 = "${var.resource_prefix}-nsg"
+  location             = var.web_server_location
+  resource_group_name  = azurerm_resource_group.web_server_region.name
+
+  security_rule {
+    name               = "Inbound"
+    priority           = 100
+    direction          = "Inbound"
+    access             = "Allow"
+    protocol           = "TCP"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+   tags ={
+     environment = "Production"
+   }
+ }
+
+#  resource "azurerm_network_interface_security_group_association" "web_server_nsg_association" {  # to link security group to the network interface. 
+#    network_interface_id          = azurerm_network_interface.web_server_nic.id
+#    network_security_group_id     = azurerm_network_security_group.web_server_nsg.id
+
+#  }
 
--- main/vars.tf
+++ main/vars.tf
@@ -21,3 +21,13 @@ variable "web_server_address_prefix" {
     type    = string
     default = "1.0.1.0/24"
  }
+
+ variable "web_server_name" {
+    type    = string
+    default = "web-01"
+ }
+
+ variable "environment" {
+    type    = string
+    default = "Production"
+ }
